76a46b5a5e0713fb856c304aba3987e28f464b10
Gabriel Tardochi <ga.tardochisalles@gmail.com>
2023-09-23 20:44:53 -0300

    black code styling

--- supertriplets/models/__init__.py
+++ supertriplets/models/__init__.py
@@ -1,8 +1,3 @@
 from ._loader import load_pretrained_model
 
-__all__ = [
-    "load_pretrained_model",
-    "language",
-    "multimodal",
-    "vision"
-]
+__all__ = ["load_pretrained_model", "language", "multimodal", "vision"]
