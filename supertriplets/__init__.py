c2159eb8620cb6735cdcd8c24fc1ff2beff9771e
Gabriel Tardochi <ga.tardochisalles@gmail.com>
2023-09-23 21:07:58 -0300

    fix missing supertriplets.models in dist

--- supertriplets/__init__.py
+++ supertriplets/__init__.py
@@ -1,3 +1,1 @@
 __version__ = "1.0.3"
-
-__all__ = ["models", "dataset", "distance", "encoder", "evaluate", "index", "loss", "sample", "utils"]
--- supertriplets/models/__init__.py
+++ supertriplets/models/__init__.py
@@ -1,3 +1,1 @@
 from ._loader import load_pretrained_model
-
-__all__ = ["load_pretrained_model", "language", "multimodal", "vision"]
